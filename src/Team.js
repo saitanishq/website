bc43047e545c49118e462cc05b2cf6ed1ca28561
snannap <snannap@iu.edu>
2024-03-28 12:51:16 -0400

    Kendal-Image

--- src/Team.js
+++ src/Team.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Team.css';
-import teamMemberImage from './Tanishq1.png'; // Ensure this is the correct path to your image
+import teamMemberImage from './Tanishq1.png';
+import teamMemberImage2 from './Kendal.png'; // Ensure this is the correct path to your image
 
 const Team = () => {
   return (
@@ -13,7 +14,7 @@ const Team = () => {
         </div>
       </div>
       <div className="bubble">
-        <img src={teamMemberImage} alt="Team Member" className="bubble-image" />
+        <img src={teamMemberImage2} alt="Team Member" className="bubble-image" />
         <div className="overlay-text"> 
           <p className="name">Kendal Merry</p>
           <p className="designation">Co-Founder</p>
